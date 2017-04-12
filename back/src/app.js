58496b2ef7845f6066e11a713b0289e4bfa58305
Nayef Ghattas <nayef.ghattas@gmail.com>
2017-04-12 09:45:25 +0200

    feat: add commun modules for koa

--- back/src/app.js
+++ back/src/app.js
@@ -1,9 +1,27 @@
 import Koa from 'koa';
+import morgan from 'koa-morgan';
+import bodyParser from 'koa-bodyparser';
+import compress from 'koa-compress';
+import helmet from 'koa-helmet';
+import cors from 'kcors';
+
+import { config } from 'dotenv';
+
+config();
+
+const debug = require('debug')('onboard-fridge');
 
 const app = new Koa();
+
+app.use(compress());
+app.use(app.env === 'development' ? morgan('dev') : morgan('combined'));
+app.use(bodyParser());
+app.use(helmet());
+app.use(cors());
 
 app.use((ctx) => {
     ctx.body = 'Hello World';
 });
 
-app.listen(3001);
+debug('Starting server on port 3000');
+app.listen(3000);
